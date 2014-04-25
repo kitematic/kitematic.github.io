1955c5d4a0e7a29264f87264a2942bb529b8eddd
Jeffrey Morgan <jmorganca@gmail.com>
2014-04-25 17:48:30 -0400

    Disabling csslint for sass files

--- Gruntfile.js
+++ Gruntfile.js
@@ -316,7 +316,7 @@ module.exports = function (grunt) {
       check: {
         src: [
           '<%= yeoman.app %>/css/**/*.css',
-          '<%= yeoman.app %>/_scss/**/*.scss'
+          //'<%= yeoman.app %>/_scss/**/*.scss'
         ]
       }
     },
